1b35bc5e470d7da116d77b6eabe03f42b7129a3a
calavera <fmontani81@gmail.com>
2021-06-10 13:55:21 +0200

    abbozzato sass, ancora da riordinare/dividere varie view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/app.css">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 
-        <title>Bozza</title>
+    <title>Bozza</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <ul>
-            @foreach ($movies as $movie)
-            <li class="movie">
-                <ul>
-                    <li>{{ $movie['original_title'] }}</li>
-                    <li>{{ $movie['title'] }} </li>
-                    <li>{{ $movie['vote'] }}</li>
-                </ul>
-            </li>
-                
-            @endforeach
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+</head>
 
-        </ul>
-    </body>
+<body>
+    <main>
+        <h2>Programmazione serale</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Titolo</th>
+                    <th>Titolo Originale</th>
+                    <th>Voto</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>
+                            {{ $movie['original_title'] }}
+                        </td>
+                        <td>
+                            {{ $movie['title'] }}
+                        </td>
+                        <td>
+                            {{ $movie['vote'] }}
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </main>
+</body>
+
 </html>
