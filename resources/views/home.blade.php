26ebf8e83a21c52669fa4ef1065a08576c78c179
calavera <fmontani81@gmail.com>
2021-06-10 16:46:52 +0200

    cambiato voto in table da valore numerico a grafico (5 stelline)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,40 @@
 @extends('layouts.app')
 @section('main_content')
-@include('components.covid', [
+    @include('components.covid', [
     'top' => '10px',
     'left' => '50%'
-])
-<h2>Programmazione serale</h2>
-<table>
-    <thead>
-        <tr>
-            <th>Titolo</th>
-            <th>Titolo Originale</th>
-            <th>Voto</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($movies as $movie)
+    ])
+    <h2>Programmazione serale</h2>
+    <table>
+        <thead>
             <tr>
-                <td>
-                    {{ $movie['original_title'] }}
-                </td>
-                <td>
-                    {{ $movie['title'] }}
-                </td>
-                <td>
-                    {{ $movie['vote'] }}
-                </td>
+                <th>Titolo</th>
+                <th>Titolo Originale</th>
+                <th>Voto</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($movies as $movie)
+                <tr>
+                    <td>
+                        {{ $movie['original_title'] }}
+                    </td>
+                    <td>
+                        {{ $movie['title'] }}
+                    </td>
+                    <td>
+                        <span class="vote"
+                            style="background-image: linear-gradient(to right, #dad61b {{ $movie['vote'] * 10 }}%, #fdfdfd 10% )">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </span>
+                    </td>
+                </tr>
+
+            @endforeach
+        </tbody>
+    </table>
 @endsection
